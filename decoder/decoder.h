a360d98bf6c8b2c629dd58a0f3848bb84c7469b1
Arkady Siuris <arss860@gmail.com>
2021-11-09 17:48:06 +0300

    task done

---
+++ decoder/decoder.h
@@ -0,0 +1,9 @@
+#ifndef TEORCOD_DECODER_H
+#define TEORCOD_DECODER_H
+
+#include "../huffman_tree.h"
+#include "../preprocess.h"
+
+std::string decode(std::string &encoded_input);
+
+#endif //TEORCOD_DECODER_H
---
+++ main.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <cassert>
+#include <filesystem>
+
+#include "preprocess.h"
+#include "encoder/encoder.h"
+#include "utils.h"
+#include "decoder/decoder.h"
+
+using namespace std;
+
+encode_report encode_decode_test(string &&test_message) {
+    auto[encoded, report] = encode(test_message, true);
+    string decoded = decode(encoded);
+
+    assert(decoded == test_message);
+
+    return report;
+}
+
+void encode_decode_simple_test() {
+    string test_message = "IF_WE_CANNOT_DO_AS_WE_WOULD_WE_SHOULD_DO_AS_WE_CAN";
+    cout << "Tesing on: " << test_message << '\n';
+    encode_decode_test(std::move(test_message));
+}
+
+void write_read_file_test() {
+    cout << "Testing read/write\n";
+
+    string test_message = "IF_WE_CANNOT_DO_AS_WE_WOULD_WE_SHOULD_DO_AS_WE_CAN";
+    auto[encoded, report] = encode(test_message, true);
+    write_to_file(test_message, "test.txt");
+
+    string from_file = read_file_to_string("test.txt");
+    string decoded_from_file = decode(from_file);
+    string decoded = decode(encoded);
+
+    assert(test_message == decoded);
+}
+
+int main() {
+    write_read_file_test();
+    encode_decode_simple_test();
+    cout << '\n';
+
+
+    string csv_report;
+    bool csv = true;
+    if (csv) {
+        cout << encode_report::csv_header << '\n';
+        csv_report += encode_report::csv_header;
+        csv_report += '\n';
+    }
+
+    size_t total_bytes = 0;
+    for (const auto &entry: filesystem::directory_iterator("../test")) {
+        if (!csv) {
+            cout << "Testing file: " << entry.path().filename() << '\n';
+        }
+        string file_content = read_file_to_string(entry.path().string());
+        auto report = encode_decode_test(std::move(file_content));
+
+        total_bytes += report.total_bits_cnt / 8 + (report.total_bits_cnt % 8 != 0);
+
+        cout << (csv ? report.csv(entry.path().filename().string()) : report.show()) << '\n';
+        if (csv) {
+            csv_report += report.csv(entry.path().filename().string());
+            csv_report += '\n';
+        }
+    }
+    cout << "Total bytes: " << total_bytes;
+
+    if (csv) {
+        write_to_file(csv_report, "../report.csv");
+    }
+}
